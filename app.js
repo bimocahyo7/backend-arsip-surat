fbe247ef3bece7c73ae238bd72fef3c4250ff8f7
bimocahyo7 <bimochayo@gmail.com>
2024-07-11 21:41:48 +0700

    Add library serverless http

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routers = require("./routes/index");
 const cors = require("cors");
+const serverless = require("serverless-http");
 
 const app = express();
 const port = 4000;
@@ -15,6 +16,15 @@ app.get("/", (req, res) => {
 
 app.use(routers);
 
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 app.listen(port, () => {
   console.log(`Started server at on port ${port}`);
 });
+
+module.exports = app;
+module.exports.handler = serverless(app);
